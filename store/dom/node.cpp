9fbc901f9e00cd9ee6f1b6595e375e8c7fb32df8
e1turin <ivturnik2003@gmail.com>
2024-01-06 01:59:03 +0300

    interesting idea of data managing. I'll give it up as I need to create in simpler.

--- store/dom/node.cpp
+++ store/dom/node.cpp
@@ -1,2 +1,15 @@
 
 #include "node.h"
+
+namespace dom {
+
+Value::Type Value::getType() const {
+  return Value::Type::kBoolean;
+}
+
+Int32Value::Int32Value(int32_t) {}
+BoolValue::BoolValue(bool) {}
+Float64Value::Float64Value(double) {}
+StringValue::StringValue(std::string) {}
+
+}
--- store/dom/node.h
+++ store/dom/node.h
@@ -1,17 +1,18 @@
 #ifndef ITMO_LLP_STORE_DOM_NODE_H_
 #define ITMO_LLP_STORE_DOM_NODE_H_
 
-#include "cstdint"
+#include <cstdint>
+#include <string>
 
 namespace dom {
-class Value {
+class alignas(8) Value {
  public:
   enum class Type {
-    Int32,
-    Float64,
-    String,
-    Boolean,
-    Object,
+    kInt32,
+    kFloat64,
+    kString,
+    kBoolean,
+    kObject,
   };
 
   [[nodiscard]] Type getType() const;
@@ -26,18 +27,65 @@ class Value {
     static_assert(this->is<T>());
     return *reinterpret_cast<const T*>(this);
   }
+
+ protected:
+  //
+  enum class Tag : uint8_t {
+    kShortString = 0b0000'0000,  // inline payload
+    kInt32       = 0b0000'0001,  // inline payload
+    kFloat32     = 0b0000'0010,  // inline payload
+    kBoolean     = 0b0000'0011,  // inline payload
+    kObject      = 0b0000'0100,  // offset to store place
+    kString      = 0b0000'0101,  // offset to store place
+  };
+  inline static constexpr uint8_t kTagMask = 0b0000'0111;
+
+  /* Value (like in Skia: skjson::Value):
+   * +------------------------------------------------------------+
+   * | Tag |      remaining data (inline/offset by 64 bit)        |
+   * +------------------------------------------------------------+
+   */
+
+ private:
+  inline static constexpr size_t kValueSize = 8;  // Value type is 64 bit size and lower bits are used for tag.
+                                                  // For referencing data of size bigger than 64 bit remaining 61 bit
+                                                  // can be used after excluding tag value (addressing by 8 byte).
+  uint8_t fData[kValueSize];
 };
 
 class Int32Value final : Value {
-  inline static constexpr Type type = Type::Int32;
+  inline static constexpr Type type = Type::kInt32;
 
   explicit Int32Value(int32_t);
 };
 
 class BoolValue final : Value {
-  inline static constexpr Type type = Type::Boolean;
+  inline static constexpr Type type = Type::kBoolean;
 
   explicit BoolValue(bool);
+};
+
+class Float64Value final : Value {
+  inline static constexpr Type type = Type::kFloat64;
+
+  explicit Float64Value(double);
+};
+
+class StringValue final : Value {
+  inline static constexpr Type type = Type::kString;
+
+  explicit StringValue(std::string);
+};
+
+struct Member {
+  StringValue fKey;
+  Value fValue;
+};
+
+class ObjectValue final : Value {
+  inline static constexpr Type type = Type::kObject;
+
+  //
 };
 
 // TODO
