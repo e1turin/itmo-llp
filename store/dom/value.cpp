1c7b0d5668938200fccbbb39b6f63d2c54173e5e
e1turin <ivturnik2003@gmail.com>
2024-02-21 01:51:14 +0300

    impl getters for primitives

--- store/dom/value.cpp
+++ store/dom/value.cpp
@@ -11,6 +11,7 @@ void Value::init_tagged(Tag t) {
 }
 
 void Value::init_tagged_pointer(const Tag t, void *p) {
+  //TODO inline tag
   init_tagged(t);
   *(cast_data<uintptr_t>()) = reinterpret_cast<uintptr_t>(p);
 }
@@ -24,20 +25,26 @@ BoolValue::BoolValue(bool b) {
   *(cast_data<bool>()) = b;
 }
 
-bool BoolValue::get_bool() const {}
+bool BoolValue::get_bool() const {
+  return *cast_data<bool>();
+}
 
 Int32Value::Int32Value(int32_t i) {
   init_tagged(Tag::kInt32);
   *(cast_data<int32_t>()) = i;
 }
 
-std::int32_t Int32Value::get_int() const {}
+std::int32_t Int32Value::get_int() const {
+  return *cast_data<std::int32_t>();
+}
 
 Float32Value::Float32Value(float f) {
   init_tagged(Tag::kFloat32);
   *(cast_data<float>()) = f;
 }
 
-float Float32Value::get_float() const {}
+float Float32Value::get_float() const {
+  return *cast_data<float>();
+}
 
 } // namespace dom
